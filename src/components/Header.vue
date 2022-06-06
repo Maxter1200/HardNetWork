404be590339b6ce70e39f49e352379488055edaf
unknown <vivasmatias@hotmail.com>
2022-06-06 11:33:39 -0300

    buscar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <center></center>
+  <center><input class="search_box" placeholder="¿Qué estás buscando?"></center>
 </template>
 
 <script>
@@ -14,7 +14,14 @@ export default {
     width: 30%;
     margin: 20px;
     padding: 10px;
-    font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif
+    font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    border: 1px solid rgb(15, 30, 160);
+    border-radius: 20px;
+}
+
+.search_box:focus{
+    border: 2px solid rgb(15, 30, 160);
+    transition: 0.5s;
 }
 
 </style>
